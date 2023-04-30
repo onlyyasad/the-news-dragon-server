18e99ec4193e3cd46e4cfd2f91d8bc1021eb3576
onlyyasad <onlyyasad@gmail.com>
2023-04-30 23:01:12 +0600

    server code is ready

--- index.js
+++ index.js
@@ -2,19 +2,41 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 
-const cors = require('cors')
+const cors = require('cors');
 
-const categories = require('./data/categories.json')
+const categories = require('./data/categories.json');
+const news = require('./data/news.json')
 
 app.use(cors());
 
 app.get("/", (req, res) =>{
-    res.send("Hello World")
+    res.send("Dragon is Running")
 });
+
+app.get("/news", (req, res) =>{
+    res.send(news)
+})
+
+app.get("/news/:id", (req, res) =>{
+    const id = req.params.id;
+    const singleNews = news.find(n => n._id === id);
+    res.send(singleNews)
+})
 
 app.get('/categories', (req, res) =>{
     res.send(categories)
 })
+
+app.get('/categories/:id', (req, res) =>{
+    const id = parseInt(req.params.id);
+    if(id === 0) {
+        res.send(news)
+    }
+    else{
+        const categoryNews = news.filter(n => parseInt(n.category_id) === id)
+        res.send(categoryNews)
+    }
+} )
 
 app.listen(port, () =>{
     console.log(`This server is running in: ${port}`)
